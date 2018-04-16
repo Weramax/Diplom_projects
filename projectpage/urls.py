0f41efd237b3f2461f3b83b3ce48617c4b2b6a02
Weramax <minnikov5422@gmail.com>
2018-04-16 19:55:51 +0400

    pdf
    create pdf

--- projectpage/urls.py
+++ projectpage/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .views import main, details, create_project, create_project_task
+from .views import main, details, create_project, create_project_task, GeneratePDF
 
 urlpatterns = [
 	url(r'^$', main),
 	url(r'^project/(?P<pk>\d+)$', details),
 	url(r'^project/createproject$', create_project),
-	url(r'^(?P<pk>\d+)/create_project_task$', create_project_task)
+	url(r'^(?P<pk>\d+)/create_project_task$', create_project_task),
+	url(r'^(?P<pk>\d+)/pdf/$', GeneratePDF.as_view())
 ]
--- projectpage/views.py
+++ projectpage/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render, redirect
-from django.db.models import Q
 from .models import Project, Species_project, Species_Task
+from django.contrib.auth.models import User
+from django.template.loader import get_template
+from django.http import HttpResponse
+from django.views.generic import View
+
+from .utils import render_to_pdf
+
+
+class GeneratePDF(View):
+	def get(self, request, pk, *args, **kwargs):
+
+		
+		template = get_template('invoice.html')
+		
+		context = {
+			"project": Project.objects.filter(species_id=pk),
+			"species_project": Species_project.objects.get(id=pk)
+         }
+		html = template.render(context)
+		pdf = render_to_pdf('invoice.html', context)
+		if pdf:
+			response = HttpResponse(pdf, content_type='application/pdf')
+			filename = "Invoice_%s.pdf" %("12341231")
+			content = "inline; filename='%s'" %(filename)
+			download = request.GET.get("download")
+			if download:
+				content = "attachment; filename='%s'" %(filename)
+			response['Content-Disposition'] = content
+			return response
+		return HttpResponse("Not found")
 
 
 def main(request):
@@ -11,9 +40,8 @@ def main(request):
 
 def details(request, pk):
 	project = Project.objects.filter(species_id=pk)
-	
+		
 	species_projects = Species_project.objects.get(id=pk)
-	
 
 	return render(request, 'projects/details_project.html', {'project':project, 'species_projects':species_projects})
 
@@ -30,16 +58,15 @@ def create_project(request):
 
 def create_project_task(request, pk):
 	task_species = Species_Task.objects.all()
-	#users_task = auth_user()
-	#for user_task in users_task:
-	#	user_task.groups.all()
+	user_list = User.objects.all()
 
 	if request.method == 'POST':
 		name = request.POST.get('name')
 		description = request.POST.get('description')
 		species_task = request.POST.get('species_task')
 		date_finish = request.POST.get('finish_date')
-		Project.objects.get_or_create(name = name, species_id=pk, species_task_id=species_task, finish_task=date_finish, description=description)
-		return redirect('/project/'+pk)
+		user_task = request.POST.get('user_task')
+		Project.objects.get_or_create(name = name, species_id=pk, species_task_id=species_task, user=user_task, finish_task=date_finish, description=description)
+		
 
-	return render(request, 'projects/create_project_task.html', {'task_species': task_species})
+	return render(request, 'projects/create_project_task.html', {'task_species': task_species, 'user_list':user_list})
